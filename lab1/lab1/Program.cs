f5f8e4a89430163f684b58ea27a8eb7473320a15
evgenijaZ <evg.zubrich@gmail.com>
2017-06-02 16:03:11 +0300

    tests for Graph

---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph a = new Graph();
+            a.Read();
+            a.PrintList();
+            a.PrintMatrix();
+            Console.ReadKey();
+        }
+    }
+}
