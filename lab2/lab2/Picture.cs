3aa222441dd1614cfd0db11493e49f03133cf1ba
evgenijaZ <evg.zubrich@gmail.com>
2017-06-05 19:50:21 +0300

    added class Picture

---
+++ lab2/lab2/Picture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class Picture
+    {
+        public List<Triangle> figures = new List<Triangle>();
+
+        public override string ToString()
+        {
+            String result = "";
+            foreach(Triangle t in figures){
+                result += t.GetType() + " {\n" + t.ToString() + " }\n";
+            }
+            return result;
+        }
+
+       
+    }
+}
