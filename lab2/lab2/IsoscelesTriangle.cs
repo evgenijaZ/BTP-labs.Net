83009471e35dbe46c7038436262db60a8bbbf144
evgenijaZ <evg.zubrich@gmail.com>
2017-06-05 16:15:49 +0300

    added class IsoscelesTriangle

---
+++ lab2/lab2/IsoscelesTriangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class IsoscelesTriangle : Triangle
+    {
+        public override double Perimeter()
+        {
+            return this.SideA * 2 + 2 * this.SideA * Math.Sin(this.Angle / 2);
+        }
+
+        public override double Square()
+        {
+            return Math.Pow(this.SideA, 2) * 0.5 * Math.Sin(this.Angle);
+        }
+    }
+}
