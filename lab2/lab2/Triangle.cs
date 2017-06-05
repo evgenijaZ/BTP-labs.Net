29444bd2172a239ba341636114ba08305195162a
evgenijaZ <evg.zubrich@gmail.com>
2017-06-05 15:20:37 +0300

    added abstract class triangle

---
+++ lab2/lab2/Triangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    abstract class Triangle
+    {
+        private double sideA;
+        private double sideB;
+        private double angle;
+
+        public double SideA
+        {
+            get { return sideA; }
+            set { sideA = value; }
+        }
+        public double SideB
+        {
+            get { return sideB; }
+            set { sideB = value; }
+        }
+        public double Angle
+        {
+            get { return angle; }
+            set { angle = value; }
+        }
+
+        abstract public double Square();
+        abstract public double Perimeter();
+
+    }
+}
