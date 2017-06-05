f9e31c2329057312e3c6818b36c2744ec6455fc7
evgenijaZ <evg.zubrich@gmail.com>
2017-06-05 16:16:12 +0300

    added class RightTriangle

---
+++ lab2/lab2/RightTriangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class RightTriangle : Triangle
+    {
+        public override double Perimeter()
+        {
+            return this.SideA + this.SideB + this.SideB *Math.Sin(this.Angle);
+        }
+
+        public override double Square()
+        {
+            return this.SideA * this.SideB * 0.5;
+        }
+    }
+}
