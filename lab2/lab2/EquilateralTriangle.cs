32d56aed682dc14e650eee6788c0cccde4db091a
evgenijaZ <evg.zubrich@gmail.com>
2017-06-05 16:15:25 +0300

    added class EquilateralTriangle

---
+++ lab2/lab2/EquilateralTriangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class EquilateralTriangle : Triangle
+    {
+     
+        public override double Perimeter()
+        {
+            return this.SideA * 3;
+        }
+
+        public override double Square()
+        {
+            return Math.Pow(this.SideA, 2) * Math.Pow(3, 0.5) / 4;
+        }
+    }
+}
