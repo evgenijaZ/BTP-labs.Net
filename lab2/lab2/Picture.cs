7d8765ec90d97f467978a166fab0c5d1159df77c
evgenijaZ <evg.zubrich@gmail.com>
2017-06-06 12:35:28 +0300

    added exception handling, Remove(), RemoveAt()

--- lab2/lab2/Picture.cs
+++ lab2/lab2/Picture.cs
@@ -12,13 +12,39 @@ namespace lab2
 
         public delegate void MethodContainer(String message);
         public event MethodContainer onAdd;
+        public event MethodContainer onRemove;
 
-        public void Add(Triangle t) {
+        public void Add(Object obj) {
+            if (obj == null)
+            {
+                 throw new ArgumentNullException("Argument can`t be null");
+            }
+
+            Triangle t = obj as Triangle;
+            if ((System.Object)t == null)
+            {
+                throw new ArgumentNullException("Argument can`t be null");
+            }
+
             figures.Add(t);
-            onAdd?.Invoke("____________________________________\nAdded new element\n" + t.ToString());
+            onAdd?.Invoke("\nAdded new element\n" + t.ToString()+"\n");
         }
 
-       
+        public bool Remove(Triangle t) {
+            bool result = figures.Remove(t);
+            if (result) onRemove?.Invoke("\nRemoved element\n" + t.ToString());
+            return result;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index >= figures.Count)
+                throw new MyIndexOutOfRangeException("Index (" + index + ") can`t be out of range ( size="+figures.Count+" )");
+            figures.RemoveAt(index);
+            onRemove?.Invoke("\nRemoved element at "+index);
+            
+        }
+
         public override string ToString()
         {
             String result = "";
