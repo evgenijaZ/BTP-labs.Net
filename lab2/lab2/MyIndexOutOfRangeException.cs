9b06ee48828093709f3aaa68db98e476e68bcafc
evgenijaZ <evg.zubrich@gmail.com>
2017-06-06 12:32:30 +0300

    added exceptions

---
+++ lab2/lab2/MyIndexOutOfRangeException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class MyIndexOutOfRangeException : Exception
+    {
+        public MyIndexOutOfRangeException()
+        {
+        }
+
+        public MyIndexOutOfRangeException(string message) : base(message)
+        {
+        }
+
+        public MyIndexOutOfRangeException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected MyIndexOutOfRangeException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ lab2/lab2/MyStackOverflowException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class MyStackOverflowException : Exception
+    {
+        public MyStackOverflowException()
+        {
+        }
+
+        public MyStackOverflowException(string message) : base(message)
+        {
+        }
+
+        public MyStackOverflowException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected MyStackOverflowException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
